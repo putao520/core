6f805986b7b3b4089927fe3c7cd364611e4c55bf
putao520 <yuyao1022@hotmail.com>
2021-04-14 19:24:52 +0800

    修改测试用例

--- test/testDbAndCache.java
+++ test/testDbAndCache.java
@@ -1,16 +1,18 @@
 import common.java.Cache.CacheHelper;
 import common.java.Thread.ThreadHelper;
 import junit.framework.TestCase;
+import org.junit.Test;
 
 
 public class testDbAndCache extends TestCase {
-    public void TestCacheLocal() {
-        // 测试不带配置名本地缓存
+    // 测试不带配置名本地缓存
+    @Test
+    public void testCacheLocal() {
         CacheHelper ca = CacheHelper.buildLocalCache();
         ca.set("TestA", "putao520");
-        assertEquals(ca.get("TestA"), "putao520", "写入缓存失败");
+        assertTrue(ca.get("TestA").equals("putao520"));
         ca.setExpire("TestA", 1000);    // 设置缓存过期时间1秒
         ThreadHelper.sleep(3000);
-        assertEquals(ca.get("TestA"), null, "过期设置无效");
+        assertTrue(ca.get("TestA") == null);
     }
 }
